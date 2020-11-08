d6c5d79e4f36a0c1d6818ba6f13b532735852b19
Ali Haynes <70223502+avhaynes89@users.noreply.github.com>
2020-11-08 16:09:08 -0600

    Removed static HTML for forecast and moved into JS

--- src/index.js
+++ src/index.js
@@ -22,6 +22,19 @@ function formatDate(date) {
 
   return `${day} ${hours}:${minutes}`;
 }
+function formatHours(timestamp){
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${hours}:${minutes}`;
+}
 function showCelciusTemp(event) {
   event.preventDefault();
   celciusLink.classList.add("active");
@@ -65,10 +78,31 @@ function showWeather(response) {
   document.querySelector("#icon").setAttribute("alt", response.data.weather[0].description);
 }
 
+function displayForecast(response) {
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = null;
+  let forecast = null;
+  for (let index = 0; index < 6; index++) {
+      forecast = response.data.list[index];
+      forecastElement.innerHTML+= `
+            <div class="col-2">
+                <h3>
+                    ${formatHours(forecast.dt*1000)}
+                </h3>
+                <img src="http://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png" alt="">
+                <div class="weather-forecast-temperature">
+                    <strong>${Math.round(forecast.main.temp_max)}°</strong> | ${Math.round(forecast.main.temp_min)}°
+                </div>
+            </div>`;}
+}
+
 function searchCity(city) {
   let apiKey = "4bf0fe9a3eb704aa29ec59086a3a7318";
   let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(url).then(showWeather);
+
+  let apiUrl= `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function citySubmit(event) {
